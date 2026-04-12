57f0374e5c79d6d2077ae64bbc17040bbea5cfde
nhatV0 <106379348+nhatV0@users.noreply.github.com>
2026-04-12 23:31:13 +0700

    Update lesson titles and descriptions in oop.js

--- data-chuyen/oop.js
+++ data-chuyen/oop.js
@@ -4,22 +4,46 @@ window.courseContent = [
         chapterName: "Chuyên đề: Lập trình hướng đối tượng (OOP)",
         lessons: [
             { 
-                title: "Đối tượng và lớp", 
+                title: "Chương 1: Sự khác biệt giữa C++ và C", 
                 videoId: "", 
-                desc: "Trừu tượng hóa các thực thể trong bài toán.", 
-                downloadUrl: "#" 
+                desc: "Chương 1 nè he", 
+                pdfUrl: "https://drive.google.com/file/d/1h5PZuNTMKlMoUw_-VuRGVzh_0lLQmjE8/view?usp=drive_link" 
             },
             { 
-                title: "Friends & Quá tải, Khuôn hình", 
+                title: "Chương 2: Mở đầu về lập trình hướng đối tượng", 
                 videoId: "", 
-                desc: "Các kỹ thuật nâng cao để liên kết và tối ưu hóa các lớp.", 
-                downloadUrl: "#" 
+                desc: "Chương 2 Đây nò", 
+                pdfUrl: "https://drive.google.com/file/d/1m3uG3foipgDvA6DeWMXdjEC5jgCEfD6z/view?usp=drive_link" 
             },
             { 
-                title: "Đa hình & Kế thừa", 
+                title: "Chương 3: Lớp và thực thể", 
                 videoId: "", 
-                desc: "Khả năng tái sử dụng mã nguồn và quản lý các loại đối tượng khác nhau trong một hệ thống chung.", 
-                downloadUrl: "#" 
+                desc: "Chương 3 đó nhoa", 
+                pdfUrl: "https://drive.google.com/file/d/1m_rCE7ehp4lqV9RAiNFYl2G6uac_kfEV/view?usp=drive_link" 
+            },
+            { 
+                title: "Chương 4: Cài đặt phép toán ", 
+                videoId: "", 
+                desc: "Chương 4 rồi đó", 
+                pdfUrl: "https://drive.google.com/file/d/19VD8an2wRJR3dxjHHqW4xfFOnBfcr5NG/view?usp=drive_link" 
+            },
+            { 
+                title: "Chương 5: Tính Kế Thừa", 
+                videoId: "", 
+                desc: "Chương 5 rồi nòa", 
+                pdfUrl: "https://drive.google.com/file/d/1vpNKM_pFcBuXFzlDaEh5ebHGWVg7PtGx/view?usp=drive_link" 
+            },
+            { 
+                title: "Chương 6: Sự nối kết động và tính đa hình", 
+                videoId: "", 
+                desc: "Chương sixxxxxxxxxxxxxxx", 
+                pdfUrl: "https://drive.google.com/file/d/1lueB0Z2QgRaULn09-S716fxUUMu9tSA_/view?usp=drive_link" 
+            },
+            { 
+                title: "Chương 7: Các dòng nhập và xuất ", 
+                videoId: "", 
+                desc: "End rồi", 
+                pdfUrl: "https://drive.google.com/file/d/1d0Q1WCsKNNrVouxbFeTo6FcnnpB_AsCd/view?usp=drive_link" 
             }
         ]
     }
